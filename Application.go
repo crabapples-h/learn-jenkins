f187a50e855d173b085508c8181f556b11c9d74d
unknown <crabapples.cn@gmail.com>
2023-03-24 01:16:45 +0800

    test

--- Application.go
+++ Application.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello,world")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":9999", nil)
+}
+func index(response http.ResponseWriter, request *http.Request) {
+	data := fmt.Sprintf("%s:%s", "hello world1", time.Now().Format("2006-01-02 15:04:05"))
+	response.Write([]byte(data))
 }
