04e9da88b9ac682dc21c158b10c123e59466ba35
unknown <crabapples.cn@gmail.com>
2023-03-24 01:28:30 +0800

    test

--- Application.go
+++ Application.go
@@ -11,6 +11,6 @@ func main() {
 	http.ListenAndServe(":9999", nil)
 }
 func index(response http.ResponseWriter, request *http.Request) {
-	data := fmt.Sprintf("%s:%s", "hello world", time.Now().Format("2006-01-02 15:04:05"))
+	data := fmt.Sprintf("%s:%s", "hello world1", time.Now().Format("2006-01-02 15:04:05"))
 	response.Write([]byte(data))
 }
